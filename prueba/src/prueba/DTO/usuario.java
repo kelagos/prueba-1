a102b51350bbd3be747234e756d596a08ba352b7
Usuario <Usuario@Noir>
2022-09-15 18:53:02 -0300

    commit prueba

---
+++ prueba/src/prueba/DTO/usuario.java
@@ -0,0 +1,16 @@
+
+package prueba.DTO;
+
+
+public class usuario {
+    private int Id;
+    private int Run;
+    private String Dv;
+    private String NombreUsuario;
+    private String Mail;
+    private int Edad;
+    private int Telefono;
+    
+    
+    
+}
