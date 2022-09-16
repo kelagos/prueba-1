051ede4e114af06eb3e4ecb47d0787de7d607672
Usuario <Usuario@Noir>
2022-09-15 21:46:58 -0300

    import main

--- prueba/src/prueba/prueba1.java
+++ prueba/src/prueba/prueba1.java
@@ -1,9 +1,17 @@
 
 package prueba;
 
+import prueba.DTO.club;
+import prueba.DTO.subscripcion;
+import prueba.DTO.usuario;
+
+
 public class prueba1 {
 
     public static void main(String[] args) {
+        
+       
+        
 
     }
     
